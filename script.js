5d13dcf364de1e0c9239d8e79444c8b60dec9c23
Abisha K <168981676+abishakla@users.noreply.github.com>
2024-12-20 09:33:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function facebook(){
         function youtube(){
             window.open("https://youtube.com/@abiuiuxdesigner?si=Bg9pSDwuCm2ZpxjA")}
             function res(){
-                window.open("file:///C:/Desktop/ABISHA%20RESUMAE.pdf")
+                window.open("https://drive.google.com/file/d/1p72LYJIb7_u8KfSk0UlbZI6lRmlqssQI/view?usp=sharing")
             }
             function li(){
               window.open("https://github.com/abishakla")
